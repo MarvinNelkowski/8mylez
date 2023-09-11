8964f357a5042f30ea6d6eca67c74e9e01c486e1
MarvinNelkowski <144095697+MarvinNelkowski@users.noreply.github.com>
2023-09-11 07:55:45 +0200

    Update aufgabe5.js

--- aufgabe5.js
+++ aufgabe5.js
@@ -1,10 +1,8 @@
 function checkPalindrome(word) {
     var reverseWord = '';
-    //i darf nicht ins minus gehen (vorher: i = 0), sondern am index von dem letzten Buchstaben anfangen
     for (var i = word.length-1; i >= 0; i--) {
     reverseWord += word[i];
     }
-    //true und false waren vertauscht
     if (reverseWord === word) {
     return true;
     } else {
